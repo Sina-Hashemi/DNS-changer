380a9aaa76af027ed6f2e98670f99b40499ce0d7
Sina Hashemi <hashemisina6@gmail.com>
2024-09-15 10:21:34 +0330

    first commit

---
+++ dns-setup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Check if an argument is provided
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <dns_name>"
+    exit 1
+fi
+
+name=$1
+
+dnses=( "empty:empty"
+        "electro:78.157.42.101 78.157.42.100"
+        "403:10.202.10.202 10.202.10.102"
+)
+
+for dns in "${dnses[@]}"; do
+    KEY="${dns%%:*}"
+    VALUE="${dns##*:}"
+    if [ "$name" == "$KEY" ]; then
+        networksetup -setdnsservers Wi-Fi $VALUE
+        echo "DNS set to $name"
+        exit 0
+    fi
+done
+
+echo "Invalid DNS name. Available options:"
+for dns in "${dnses[@]}"; do
+    KEY="${dns%%:*}"
+    echo "$KEY"
+done
+exit 1
